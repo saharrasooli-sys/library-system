598ad3c25380cb220cbb718849ff2f4693ef6082
Husnia <husnia.qaderi@auaf.edu.af>
2026-05-10 20:34:52 +0430

    Completed frontend UI and PDF integration

---
+++ models.py
@@ -0,0 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    is_available = db.Column(db.Boolean, default=True)
+
+class Checkout(db.Model):
+    __tablename__ = 'checkouts'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+
+    checkout_date = db.Column(db.DateTime, default=datetime.utcnow)
